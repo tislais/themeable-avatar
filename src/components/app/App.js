bd18dd0abad91337a655ff948b41a009b2707afc
Tis Lais <tistististis@gmail.com>
2021-08-12 17:12:35 -0700

    Add empty header component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,8 +1,12 @@
 import CharacterList from "../characters/CharacterList";
+import Header from "../header/Header";
 
 function App() {
   return (
-    <CharacterList />
+    <>
+      <Header />
+      <CharacterList />
+    </>
   );
 }
 
